667f9bbc49baa7b366a155b4a2d7033eca1bfb38
tonycherednichenko <tony.cherednichenko95@yandex.ru>
2022-11-29 22:09:09 +0300

    add sidebar feature for pages

--- src/pages/pages.model.ts
+++ src/pages/pages.model.ts
@@ -2,12 +2,12 @@
 import { Column, Table, DataType, Model } from "sequelize-typescript";
 import { IFeature, IPagesAttrs } from "./types";
 
-@Table({ tableName: 'pages' })
+@Table({ tableName: 'pages', createdAt: false, updatedAt: false })
 export class Pages extends Model<Pages, IPagesAttrs> {
     @Column({ type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true })
     id: number
 
-    @Column({ type: DataType.ARRAY(DataType.JSON), unique: false, allowNull: true }  )
+    @Column({ type: DataType.JSON, unique: false, allowNull: true }  )
     features: IFeature[]
 
     @Column({ type: DataType.STRING, unique: true })
